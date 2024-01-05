7cdc2dc181ef097f585e0441516f6a690f10b9c0
yujin <108935743+yujin0708@users.noreply.github.com>
2024-01-05 21:35:31 +0900

    feat(auth): 사용자 인증 관련 API #1 #2
    - 회원가입 로직 추가
    - 로그인 로직 추가
    - 토큰 재발급 로직 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,34 +1,72 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpStatus,
+  Request,
+  UseGuards,
+  HttpCode,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { SignUpDto } from './dto/sign-up.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { SignInDto } from './dto/sign-in.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
+  /**
+   * 회원가입 API
+   * @param {SignUpDto} signUpDto
+   * @returns {Object} statusCode, message, user
+   */
+  @Post('/signup')
+  async signUp(@Body() signUpDto: SignUpDto): Promise<object> {
+    const user = await this.authService.signup(signUpDto);
+
+    return {
+      statusCode: HttpStatus.CREATED,
+      message: '회원가입에 성공했습니다.',
+      user,
+    };
   }
 
-  @Get()
-  findAll() {
-    return this.authService.findAll();
+  /**
+   * 로그인 API
+   * @param req
+   * @param {SignInDto} signInDto
+   * @returns {Object} statusCode, message, data
+   */
+  @UseGuards(AuthGuard('local'))
+  @Post('/login')
+  signIn(@Request() req: any, signInDto: SignInDto): object {
+    const data = this.authService.signIn(req.user.id);
+
+    return {
+      statusCode: HttpStatus.OK,
+      message: '로그인에 성공했습니다.',
+      data,
+    };
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
+  /**
+   * 엑세스 토큰 재발급 API
+   * @param refreshToken
+   * @returns {Object} statusCode, message, accessToken
+   */
+  @Post('/refresh')
+  async refresh(@Body('refreshToken') refreshToken: string) {
+    const accessToken = await this.authService.refreshToken(refreshToken);
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
+    return {
+      statusCode: HttpStatus.OK,
+      message: '토큰 재발급에 성공했습니다.',
+      accessToken,
+    };
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
+import { LocalStrategy } from './strategies/local.strategy';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET_KEY'),
+        signOptions: {
+          expiresIn: '1h',
+        },
+      }),
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
