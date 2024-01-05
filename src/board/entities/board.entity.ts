fc19d89d149cd7ee0988fd6c7c79d50f72b9931b
dnjs5432 <dnjs5432@naver.com>
2024-01-05 17:02:43 +0900

    feat: board entity, dto 설정

--- src/board/entities/board.entity.ts
+++ src/board/entities/board.entity.ts
@@ -1,1 +1,34 @@
-export class Board {}
+import { IsNotEmpty } from 'class-validator';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({
+  name: 'boards',
+})
+export class Board {
+  @PrimaryGeneratedColumn()
+  boardId: number;
+
+  @IsNotEmpty({ message: '입력란을 확인해 주세요.' })
+  @Column({ type: 'varchar', nullable: false })
+  boardName: string;
+
+  @IsNotEmpty({ message: '입력란을 확인해 주세요.' })
+  @Column({ type: 'varchar', nullable: false })
+  backgroundColor: string;
+
+  @IsNotEmpty({ message: '입력란을 확인해 주세요.' })
+  @Column({ type: 'text', nullable: false })
+  description: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
