8ccf829876567f0175c5e41d8b7259139c0ae9ab
minimini1212 <zjonyv12@naver.com>
2024-01-08 11:29:47 +0900

    feat(column) 컨트롤러 서비스 생성
    - 컬럼 생성 , 컬럼 순서 이동, 컬럼 이름 변경, 컬럼 삭제 API 구현

--- src/column/column.service.ts
+++ src/column/column.service.ts
@@ -1,26 +1,116 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateColumnDto } from './dto/create-column.dto';
 import { UpdateColumnDto } from './dto/update-column.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Columns } from './entities/column.entity';
+import { DataSource, Repository } from 'typeorm';
+import { ChangePositionColumnDto } from './dto/changeposition-column.dto';
+import { LexoRank } from 'lexorank';
 
 @Injectable()
 export class ColumnService {
-  create(createColumnDto: CreateColumnDto) {
-    return 'This action adds a new column';
+  constructor(
+    @InjectRepository(Columns)
+    private readonly columnRepository: Repository<Columns>,
+    private readonly dataSource: DataSource,
+  ) {}
+
+  // 컬럼 생성
+  async create(createColumnDto: CreateColumnDto) {
+    const { title } = createColumnDto;
+
+    const foundColumns = await this.columnRepository.find({
+      order: {
+        position: 'ASC',
+      },
+    });
+
+    if (foundColumns.length < 1) {
+      const position = LexoRank.middle().toString();
+
+      return await this.columnRepository.save({
+        title,
+        position,
+      });
+    } else {
+      const position = LexoRank.parse(
+        foundColumns[foundColumns.length - 1].position,
+      );
+      const NewPosition = position.genNext().toString();
+
+      return await this.columnRepository.save({
+        title,
+        position: NewPosition,
+      });
+    }
   }
 
-  findAll() {
-    return `This action returns all column`;
+  // 컬럼 순서 이동
+  async changePosition(
+    id: number,
+    changePositionColumnDto: ChangePositionColumnDto,
+  ) {
+    const { position } = changePositionColumnDto;
+    const foundColumn = await this.columnRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!foundColumn) {
+      throw new NotFoundException('해당 컬럼이 존재하지 않습니다.');
+    }
+
+    await this.columnRepository.update({ id }, { position });
+
+    return await this.columnRepository.find({
+      order: {
+        position: 'ASC',
+      },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} column`;
+  // 하나의 api 안에 세 번의 db와의 접촉이 맘에 들지 않는데 어떠한 방법이 있을지 고민해보자구..
+  // 컬럼 이름 수정
+  async updateTitle(columnId: number, updateColumnDto: UpdateColumnDto) {
+    const { title } = updateColumnDto;
+    const foundColumn = await this.columnRepository.findOne({
+      where: {
+        id: columnId,
+      },
+    });
+
+    const updatedColumn = await this.dataSource.transaction(async (manager) => {
+      if (!foundColumn) {
+        throw new NotFoundException('해당 컬럼이 존재하지 않습니다.');
+      }
+
+      await manager.update(Columns, { id: columnId }, { title });
+
+      return await manager.findOne(Columns, {
+        where: {
+          id: columnId,
+        },
+      });
+    });
+    return updatedColumn;
   }
 
-  update(id: number, updateColumnDto: UpdateColumnDto) {
-    return `This action updates a #${id} column`;
-  }
+  // 컬럼 삭제
+  async remove(columnId: number) {
+    const foundColumn = await this.columnRepository.findOne({
+      where: {
+        id: columnId,
+      },
+    });
 
-  remove(id: number) {
-    return `This action removes a #${id} column`;
+    if (!foundColumn) {
+      throw new NotFoundException('해당 컬럼이 존재하지 않습니다.');
+    }
+    return await this.columnRepository.delete({ id: columnId });
   }
 }
