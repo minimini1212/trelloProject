debc21f025b2e427f8e4e0c38df4b2434197f254
precipice- <configure62@gmail.com>
2024-01-09 15:11:59 +0900

    feat: card 컨트롤러 코드 변경

--- src/card/card.module.ts
+++ src/card/card.module.ts
@@ -3,7 +3,6 @@ import { CardService } from './card.service';
 import { CardController } from './card.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Card } from './entities/card.entity';
-import { Column } from 'src/column/entities/column.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Card])],
