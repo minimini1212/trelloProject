cab8dd9642e85bdd29dc61c49259dc37fd8675c7
precipice- <configure62@gmail.com>
2024-01-10 09:37:17 +0900

    feat: 카드 데드라인/위치변경 수정

--- src/card/card.service.ts
+++ src/card/card.service.ts
@@ -1,22 +1,32 @@
-import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { CreateCardDto } from './dto/create-card.dto';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Card } from './entities/card.entity';
 import { UpdateCardDto } from './dto/update-card.dto';
+import { LexoRank } from 'lexorank';
+import { DeadlineCardDto } from './dto/deadline-card.dto';
+import { ChangePositionCardDto } from './dto/changeposition-card.dto';
 
 @Injectable()
 export class CardService {
   constructor(
     @InjectRepository(Card) private readonly cardRepository: Repository<Card>,
-  ) { }
+  ) {}
 
   async findAll(columnId: number) {
-    if (await this.cardRepository.findOneBy({ column: { id: columnId } }) === null)
-      throw new NotFoundException('해당 카드들이 존재하지 않습니다.')
+    if (
+      (await this.cardRepository.findOneBy({ column: { id: columnId } })) ===
+      null
+    )
+      throw new NotFoundException('해당 카드들이 존재하지 않습니다.');
     return await this.cardRepository.find({
       where: { column: { id: columnId } },
-    })
+    });
   }
 
   async create(createCardDto: CreateCardDto) {
@@ -26,35 +36,60 @@ export class CardService {
       description: createCardDto.description,
       backgroundColor: createCardDto.backgroundColor,
       position: createCardDto.position,
-    })
+    });
   }
 
   async delete(cardId: number) {
-    if (await this.cardRepository.findOneBy({ cardId }) === null)
-      throw new NotFoundException('해당 카드가 존재하지 않습니다.')
-    return await this.cardRepository.delete(cardId)
+    if ((await this.cardRepository.findOneBy({ cardId })) === null)
+      throw new NotFoundException('해당 카드가 존재하지 않습니다.');
+    return await this.cardRepository.delete(cardId);
   }
 
-  async update(updateCardDto: UpdateCardDto, cardId: number, managerId: number) {
-    if (await this.cardRepository.findOneBy({ cardId }) === null)
-      throw new NotFoundException('해당 카드가 존재하지 않습니다.')
+  async update(
+    updateCardDto: UpdateCardDto,
+    cardId: number,
+    managerId: number,
+  ) {
+    if ((await this.cardRepository.findOneBy({ cardId })) === null)
+      throw new NotFoundException('해당 카드가 존재하지 않습니다.');
     const cards = await this.cardRepository.findOne({
       where: { cardId: cardId },
-      relations: { manager: true }
-    })
+      relations: { manager: true },
+    });
 
     if (cards.manager.find((user) => user.id === managerId)) {
       await this.cardRepository
         .createQueryBuilder()
-        .relation(Card, "manager")
+        .relation(Card, 'manager')
         .of(cardId)
-        .remove(managerId)
-      
-  async updateDeadline(deadlineCardDto: DeadlineCardDto, cardId: number) {
+        .remove(managerId);
+    } else {
+      await this.cardRepository
+        .createQueryBuilder()
+        .relation(Card, 'manager')
+        .of(cardId)
+        .add(managerId);
+    }
+
+    return await this.cardRepository.update(cardId, updateCardDto);
+  }
+
+  async updateDeadline(
+    deadlineCardDto: DeadlineCardDto,
+    cardId: number,
+    managerId: number,
+  ) {
     if ((await this.cardRepository.findOneBy({ cardId })) === null)
       throw new NotFoundException('해당 카드가 존재하지 않습니다.');
-    await this.cardRepository.update(DeadlineCardDto, cardId);
-    return 'Card updated';
+    const cards = await this.cardRepository.findOne({
+      where: { cardId: cardId },
+    });
+
+    if (cards.manager.find((user) => user.id === managerId)) {
+      return await this.cardRepository.update(cardId, deadlineCardDto);
+    } else {
+      throw new UnauthorizedException('데드라인을 수정할 권한이 없습니다.');
+    }
   }
 
   async changePosition(
@@ -88,14 +123,20 @@ export class CardService {
         { position: prevCardPosition.genPrev().toString() },
       );
     } else {
-      await this.cardRepository
-        .createQueryBuilder()
-        .relation(Card, "manager")
-        .of(cardId)
-        .add(managerId)
+      const nextCardPosition = LexoRank.parse(nextCard.position);
+      const prevCardPosition = LexoRank.parse(prevCard.position);
 
+      await this.cardRepository.update(
+        { cardId },
+        { position: prevCardPosition.between(nextCardPosition).toString() },
+      );
     }
 
-    return await this.cardRepository.update(cardId, updateCardDto)
+    const currentCard = await this.cardRepository.findOne({
+      where: { cardId: cardId },
+    });
+    return await this.cardRepository.find({
+      where: { column: currentCard.column },
+    });
   }
 }
--- src/card/dto/deadline-card.dto.ts
+++ src/card/dto/deadline-card.dto.ts
@@ -1,8 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCardDto } from './create-card.dto';
-import { IsDate } from 'class-validator';
+import { IsDate, IsNotEmpty } from 'class-validator';
 
 export class DeadlineCardDto extends PartialType(CreateCardDto) {
+  @IsNotEmpty()
   @IsDate()
   deadline: Date;
 }
