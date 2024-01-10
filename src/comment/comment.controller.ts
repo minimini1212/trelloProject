63aadc7520f76138515b65cd7c5c8dd4a4f5caf2
precipice- <configure62@gmail.com>
2024-01-10 23:08:40 +0900

    fix: comment controller

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -13,7 +13,7 @@ import { CommentService } from './comment.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
 
-@Controller('')
+@Controller('comment')
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
 
